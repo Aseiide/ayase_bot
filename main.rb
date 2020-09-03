7407b2d7638d208bc4545b6dd807fc538be2e312
Aseiide <eiseiide@gmail.com>
2020-09-03 23:22:31 +0900

    fix

--- main.rb
+++ main.rb
@@ -55,7 +55,7 @@ post '/callback' do
       when Line::Bot::Event::MessageType::Text
          # userから送られてくるテキストを変数に格納
           @station_name = event.message["text"]
-          station_name_sym = @station_name.to_sym
+          @station_name_sym = @station_name.to_sym
           # 到着駅を綾瀬に固定してリクエストを投げる
           res1 = Net::HTTP.get(URI.parse("http://api.ekispert.jp/v1/json/search/course/light?key=#{ENV['ACCESS_KEY']}&from=#{station_code[@station_name.to_sym]}&to=22499"))
 
@@ -74,12 +74,12 @@ post '/callback' do
           doc.xpath('/html/body/div[1]/div[4]/div/div[1]/div[2]/div/table/tr[1]/td[3]/p[1]').each do |node|
             $time = node.inner_text
           end
-          if station_code.include?(station_name_sym)
+          if station_code.include?(@station_name_sym)
             message = {type: 'text',text: "次の綾瀬行の電車は#{$time}です"}
           elsif
             message = {type: 'text',text: "これは千代田線の駅ではありません。別の駅を入力してください"}
-            client.reply_message(event['replyToken'], message)
           end
+          client.reply_message(event['replyToken'], message)
       end
     end
   # Don't forget to return a successful response
