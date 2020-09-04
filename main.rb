8a1e417482b332932f916e8f11b1e7c7666b1fab
Aseiide <eiseiide@gmail.com>
2020-09-04 19:13:37 +0900

    修正

--- main.rb
+++ main.rb
@@ -81,6 +81,7 @@ post '/callback' do
       client.reply_message(event['replyToken'], message)
       client.reply_message(event['replyToken'], message1)
     end
+    end
   # Don't forget to return a successful response
   "OK"
   end
